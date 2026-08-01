4c79b8b883931126a0f7d7154c7f84f60b78f004
Siddu Nhavi <siddunhavi@gmail.com>
2026-08-01 14:25:51 +0530

    Updated all routes in App.js and installed all necessarry packages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/landingPage";
+import Authentication from "./pages/authentication";
+import Profile from "./pages/profile";
 
 import "./App.css";
 
@@ -9,6 +11,9 @@ export default function App() {
       <Router>
         <Routes>
           <Route path="/" element={<LandingPage />} />
+          <Route path="/auth" element={<Authentication />} />
+          <Route path="/auth/:authAction" element={<Authentication />} />
+          <Route path="/profile" element={<Profile />} />
         </Routes>
       </Router>
     </>
